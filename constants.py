d75d1e53e97f4c74e9544f3c578dbd530c5fce21
Andrew Vorsin <osvoxeconomics@gmail.com>
2023-11-29 10:47:21 +0300

    added parser

---
+++ constants.py
@@ -0,0 +1,3 @@
+builtin_functions = {'abs', 'print', 'dict', 'list', 'range', 'enumerate', 'input', 'len', 'map', 'min',
+                     'max', 'reversed', 'sum', 'sorted', 'hash'}
+builtin_methods = {'append', 'add', 'split', 'lower', 'upper'}
