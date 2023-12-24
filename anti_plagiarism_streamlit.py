8703409cc9a3fc3d04356da345507f5d76d3b955
Andrew Vorsin <osvoxeconomics@gmail.com>
2023-12-24 03:28:49 +0300

    minor fixes

--- anti_plagiarism_streamlit.py
+++ anti_plagiarism_streamlit.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from io import StringIO
-from anti_plagiarism import *
+from anti_plagiarism import predicting_functions, Model, give_pandas
 from backend import model
 
 st.set_page_config(page_title="Antiplagiarism checker", page_icon="👋", layout='wide')
@@ -19,8 +19,11 @@ if uploaded_file and metrics:
     if not parse_python and any(file.endswith('.py') for file in files.keys()):
         st.write('Not all files in folder have extension .py, we compare them as pure strings')
     if parse_python:
+        files_temp = {}
         for key, value in files.items():
-            files[key] = model.preprocessing_code(value)  # I checked it is safe
+            files_temp[key] = model.preprocessing_code(value)
+        files = files_temp
+        del files_temp
 
     results = model.compare(files)
     df = give_pandas(results, metrics)
